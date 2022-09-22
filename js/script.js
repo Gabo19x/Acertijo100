0043ecf8df167d5388387e9122112f321fb6c353
Gabriel A Pinzon Paez <arturopinzon19@gmail.com>
2022-09-22 13:15:51 -0500

    Actualizacion

--- js/script.js
+++ js/script.js
@@ -10,39 +10,51 @@ function EjercicioAcertijo() {
     numero = parseInt(prompt(
         "Numero de oportunidades (preferiblemente menor al numero de participantes)"
     ));
-    console.log(`participantes: ${participantes} numero: ${numero}`);
-    
+    console.log(`participantes: ${participantes} numero: ${numero}\n`);
+
     const acertijo = {}
-
+    let lista = [];
     for (let i = participantes; i > 0; i--) {
-        acertijo[i] = "vacio";
-    }
 
-    let contador = Object.entries(acertijo).length; 
-    for (let key in acertijo) {
-        for (const key2 in acertijo) {
-            
+        if (acertijo.hasOwnProperty(acertijo[i]) !== true) {
+
             let r = Math.floor((Math.random() * (participantes - 1 + 1)) + 1);
-            
-            if(acertijo.key2 !== r){
-                contador--;
-                if(contador === 0){
-                    acertijo[key] = r;
-                    contador = Object.entries(acertijo).length; 
+
+            if (lista.length === 0) {
+                acertijo[i] = r;
+                console.log(`Se agrego por vacio: ${acertijo[i]}`);
+                lista.push(r);
+            } else {
+                let tamaño = lista.length;
+                let contador = 0;
+
+                for (let valor of lista) {
+
+                    if (valor !== r) {
+                        console.log(`Diferentes ${valor} != ${r}`);
+                        contador++;
+                        
+                        if (contador === tamaño) {
+                            console.log("Se agrego \n");
+                            acertijo[i] = r;
+                            lista.push(r);
+                        }
+                    } else {
+                        console.log(`Iguales ${valor} == ${r}`);
+                    }
                 }
             }
+        } else {
+            console.log(`Ya exite: ${acertijo[i]} `);
         }
-        
-        // if(key !== r){
-        //     acertijo[key] = r;
-        //     console.log(`key: ${key} r: ${r}`); 
-        // }
-        
+
     }
 
-    
+
     console.log(acertijo);
+    console.log(lista);
     console.log("|FINALIZO|");
 }
 
-console.log(`i: `);
+console.log(` `);
+
