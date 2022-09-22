b333444ea3407274495e1f95ece424a56cb55d7f
Gabriel A Pinzon Paez <arturopinzon19@gmail.com>
2022-09-22 17:20:10 -0500

    Ultimos avances

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
 var participantes;
-var numero
+var numero;
+const acertijo = {};
+var lista = [];
 const parrafos = document.querySelectorAll(".Results p");
 
 function EjercicioAcertijo() {
     participantes = parseInt(prompt(
-        "Numero de participantes (preferiblemente multiplo de 10)"
+        "Number of participants (preferably multiple of 10) | Numero de participantes (preferiblemente multiplo de 10)"
     ));
 
     numero = parseInt(prompt(
-        "Numero de oportunidades (preferiblemente menor al numero de participantes)"
+        "Number of opportunities (preferably less than the number of participants) | Numero de oportunidades (preferiblemente menor al numero de participantes)"
     ));
+
     console.log(`participantes: ${participantes} numero: ${numero}\n`);
 
-    const acertijo = {}
-    let lista = [];
     for (let i = participantes; i > 0; i--) {
 
         if (acertijo.hasOwnProperty(acertijo[i]) !== true) {
@@ -21,17 +22,16 @@ function EjercicioAcertijo() {
             if (bandera == false) {
                 Buscar(acertijo, lista, i);
             }
-
-        } else {
-            console.log(`Ya exite: ${acertijo[i]} `);
-        }
-
+        }// else {
+        //     console.log(`Ya exite: ${acertijo[i]} `);
+        // }
     }
 
-    
     console.log(acertijo);
-    console.log(lista.sort((a, b) => a - b ));
-    console.log("|FINALIZO|");
+    lista.sort((a, b) => a - b)
+    console.log(lista);
+    
+    Encontrar();
 }
 
 function Buscar(acertijo, lista, i) {
@@ -39,35 +39,104 @@ function Buscar(acertijo, lista, i) {
 
     if (lista.length === 0) {
         acertijo[i] = r;
-        console.log(`Se agrego por vacio: ${acertijo[i]}`);
+        // console.log(`Se agrego por vacio: ${acertijo[i]}`);
         lista.push(r);
     } else {
         let tamaño = lista.length;
         let contador = 0;
         while (contador < tamaño) {
             for (let valor of lista) {
-
                 if (valor !== r) {
-                    console.log(`Diferentes ${valor} != ${r} c: ${contador}`);
+                    // console.log(`Diferentes ${valor} != ${r} c: ${contador}`);
                     contador++;
-    
+
                     if (contador === tamaño) {
-                        console.log("Se agrego \n");
+                        // console.log("Se agrego \n");
                         acertijo[i] = r;
                         lista.push(r);
                         contador = 0;
                         tamaño = lista.length;
                         return true;
                     }
-    
                 } else {
-                    console.log(`Iguales ${valor} == ${r} c: ${contador}`);
+                    // console.log(`Iguales ${valor} == ${r} c: ${contador}`);
                     r = Math.floor((Math.random() * (participantes - 1 + 1)) + 1);
                     contador = 0;
                 }
             }
         }
     }
+}
+
+function Encontrar() {
+    const resultados = {
+        aprobado: 0,
+        desaprovado: 0,
+    };
+
+    let persona_cons = 1; 
+    let persona = 1;
+    let valor = 0;
+    let contador = 1;
+    let bandera = true;
+
+    while(bandera){
+        valor = acertijo[persona];
+        
+        if(persona_cons === valor){
+            // console.log(`pc: ${persona_cons} == v: ${valor}`);
+            if(contador <= numero){
+                resultados.aprobado += 1;
+                // console.log(`${persona_cons} aprobo `);
+
+                if(persona_cons >= participantes){
+                    bandera = false;
+                    // console.log("Se termino");
+                }else{
+                    persona_cons++;
+                    persona = persona_cons;
+                    contador = 0;
+                }
+            }else{
+                resultados.desaprovado += 1;
+                // console.log(`${persona_cons} desaprobo `);
+
+                if(persona_cons >= participantes){
+                    bandera = false;
+                    // console.log("Se termino");
+                }else{
+                    persona_cons++;
+                    persona = persona_cons;
+                    contador = 0;
+                }
+            }
+        }else{
+            // console.log(`pc: ${persona_cons} != v: ${valor}`);
+            contador++;
+            persona = valor;
+        }
+        // console.log(`pc: ${persona_cons} par: ${participantes}`);
+        
+        // console.log(`pc: ${persona_cons} p: ${persona} v: ${valor} c: ${contador} b: ${bandera}`);
+    }
+
+    Mostrar(resultados);
+}
+
+
+function Mostrar(obj) {
+    parrafos[0].textContent = `• Participants who found their number before ${numero} attempts: ${obj.aprobado}`;
+    parrafos[1].textContent = `• Participants who did NOT find their number before ${numero} attempts: ${obj.desaprovado}`;
+
+    for (let valor in acertijo) {
+        const nuevo = document.createElement('P');
+        nuevo.textContent = `• Box: ${valor} value: ${acertijo[valor]}`;
+        nuevo.classList.add("White");
+
+        const ver = document.querySelector(".View");
+        ver.appendChild(nuevo);
+    }
+    
 }
 
 console.log(` `);
