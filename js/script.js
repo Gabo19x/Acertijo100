4346f35392c8ec5f21818a04cbe78ce4b728edb3
Gabriel A Pinzon Paez <arturopinzon19@gmail.com>
2022-09-23 11:20:22 -0500

    Mejoras en UI

--- js/script.js
+++ js/script.js
@@ -22,9 +22,7 @@ function EjercicioAcertijo() {
             if (bandera == false) {
                 Buscar(acertijo, lista, i);
             }
-        }// else {
-        //     console.log(`Ya exite: ${acertijo[i]} `);
-        // }
+        }
     }
 
     console.log(acertijo);
@@ -39,7 +37,6 @@ function Buscar(acertijo, lista, i) {
 
     if (lista.length === 0) {
         acertijo[i] = r;
-        // console.log(`Se agrego por vacio: ${acertijo[i]}`);
         lista.push(r);
     } else {
         let tamaño = lista.length;
@@ -47,11 +44,9 @@ function Buscar(acertijo, lista, i) {
         while (contador < tamaño) {
             for (let valor of lista) {
                 if (valor !== r) {
-                    // console.log(`Diferentes ${valor} != ${r} c: ${contador}`);
                     contador++;
 
                     if (contador === tamaño) {
-                        // console.log("Se agrego \n");
                         acertijo[i] = r;
                         lista.push(r);
                         contador = 0;
@@ -59,7 +54,6 @@ function Buscar(acertijo, lista, i) {
                         return true;
                     }
                 } else {
-                    // console.log(`Iguales ${valor} == ${r} c: ${contador}`);
                     r = Math.floor((Math.random() * (participantes - 1 + 1)) + 1);
                     contador = 0;
                 }
@@ -84,14 +78,12 @@ function Encontrar() {
         valor = acertijo[persona];
         
         if(persona_cons === valor){
-            // console.log(`pc: ${persona_cons} == v: ${valor}`);
+
             if(contador <= numero){
                 resultados.aprobado += 1;
-                // console.log(`${persona_cons} aprobo `);
 
                 if(persona_cons >= participantes){
                     bandera = false;
-                    // console.log("Se termino");
                 }else{
                     persona_cons++;
                     persona = persona_cons;
@@ -99,11 +91,9 @@ function Encontrar() {
                 }
             }else{
                 resultados.desaprobado += 1;
-                // console.log(`${persona_cons} desaprobo `);
 
                 if(persona_cons >= participantes){
                     bandera = false;
-                    // console.log("Se termino");
                 }else{
                     persona_cons++;
                     persona = persona_cons;
@@ -111,13 +101,9 @@ function Encontrar() {
                 }
             }
         }else{
-            // console.log(`pc: ${persona_cons} != v: ${valor}`);
             contador++;
             persona = valor;
         }
-        // console.log(`pc: ${persona_cons} par: ${participantes}`);
-        
-        // console.log(`pc: ${persona_cons} p: ${persona} v: ${valor} c: ${contador} b: ${bandera}`);
     }
 
     Mostrar(resultados);
@@ -125,15 +111,16 @@ function Encontrar() {
 
 
 function Mostrar(obj) {
-    parrafos[0].textContent = `• Participants who found their number before ${numero} attempts: ${obj.aprobado}`;
-    parrafos[1].textContent = `• Participants who did NOT find their number before ${numero} attempts: ${obj.desaprobado}`;
+    parrafos[0].textContent = `• Participants: ${participantes} | Allowed attempts: ${numero}`;
+    parrafos[1].textContent = `• Participants who found their number before ${numero} attempts: ${obj.aprobado}`;
+    parrafos[2].textContent = `• Participants who did NOT find their number before ${numero} attempts: ${obj.desaprobado}`;
     let pa = (obj.aprobado * 100) / participantes;
     let pd = (obj.desaprobado * 100) / participantes;
-    parrafos[2].textContent = `• Approval percentage: ${pa}% | Disapproval percentage: ${pd}%`;
-
+    parrafos[3].textContent = `• Approval percentage: ${pa}% | Disapproval percentage: ${pd}%`;
+   
     for (let valor in acertijo) {
         const nuevo = document.createElement('P');
-        nuevo.textContent = `• Box: ${valor} value: ${acertijo[valor]}`;
+        nuevo.textContent = `• Box: ${valor}  value: ${acertijo[valor]}`;
         nuevo.classList.add("White");
 
         const ver = document.querySelector(".View");
